907c7e43fcea268b13ff345de4d095457e45bc05
Cream260 <64160260@go.buu.ac.th>
2023-12-15 21:15:54 +0700

    create component card history

---
+++ lib/components/HistoryComponents.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HistoryCard extends StatelessWidget {
+  const HistoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
