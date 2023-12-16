3e9be39ed4fcb7cc58867ac8d371b7091f1cddb3
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-12-16 20:22:35 +0700

    specific click vdo

--- lib/app/modules/search_result/views/search_result_view.dart
+++ lib/app/modules/search_result/views/search_result_view.dart
@@ -1,7 +1,10 @@
 import 'dart:ffi';
 
+import 'package:ez_mooc/app/data/model/subject_model.dart';
 import 'package:ez_mooc/components/VdoComponent.dart';
+import 'package:ez_mooc/services/enrollment_service.dart';
 import 'package:ez_mooc/services/home_service.dart';
+import 'package:ez_mooc/services/subject_service.dart';
 import 'package:ez_mooc/services/vdo_detail_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -40,7 +43,35 @@ class SearchResultView extends GetView<SearchResultController> {
           : ListView.builder(
               itemCount: vdoList.length,
               itemBuilder: (context, index) {
-                return VdoComponent(vdoDetail: vdoList[index]);
+                return GestureDetector(
+                  child: VdoComponent(vdoDetail: vdoList[index]),
+                  onTap: () {
+                    Subject sub = Get.find<SubjectService>()
+                        .playlists
+                        .firstWhere((element) =>
+                            element.subjectId == vdoList[index].subjectId);
+                    //set current playlist
+                    Get.find<SubjectService>().setCurrentPlaylist(sub);
+
+                    //ser vdoplaylist
+                    Get.find<VdoDetailService>().setVdoPlaylists(sub.videos);
+                    //sert current subject
+                    Get.find<VdoDetailService>().currentSubject.value = sub;
+
+                    Get.find<VdoDetailService>().setCurrentVdo(vdoList[index]);
+
+                    print(Get.find<VdoDetailService>().getCurrentVdo().videoId);
+
+                    Get.find<EnrollmentService>().addEnrollment(sub);
+
+                    Get.find<VdoDetailService>().currentSubject = sub.obs;
+                    Get.find<EnrollmentService>().currentVdoId =
+                        vdoList[index].videoId.obs;
+                    print(
+                        'Current vdoId : ${Get.find<EnrollmentService>().currentVdoId.value}');
+                    Get.toNamed('/playlist');
+                  },
+                );
               },
             ),
     );
